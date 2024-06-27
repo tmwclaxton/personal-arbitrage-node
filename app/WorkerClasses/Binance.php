bda8636a84854a533b2c1a6e3c316e2655ada759
tmwcl <tmwclaxton@gmail.com>
2024-06-27 15:12:27 +0100

    binance flesh out

---
+++ app/WorkerClasses/Binance.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\WorkerClasses;
+
+class Binance
+{
+
+    public function buyCrypto($amount, $currency) {
+        // buy crypto from binance
+    }
+
+    public function sellCrypto($amount, $currency) {
+        // sell crypto from binance
+    }
+
+    public function checkCryptoBalance($currency) {
+        // check crypto balance from binance
+    }
+
+    public function sendBtcLightning($address, $amount) {
+        // send btc to lightning address
+
+    }
+
+}
