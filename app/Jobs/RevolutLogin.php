ac964307fe176027982f9adbe04b096b9d8c58c7
tmwcl <tmwclaxton@gmail.com>
2024-08-15 21:19:08 +0100

    fix name of container

--- app/Jobs/RevolutLogin.php
+++ app/Jobs/RevolutLogin.php
@@ -27,7 +27,7 @@ class RevolutLogin implements ShouldQueue
     public function handle(): void
     {
         # hit POST suave-container:8000/revolut-login
-        Http::post('http://suave-container:8000/revolut-login', [
+        Http::post('http://sauve-py:8000/revolut-login', [
             'auto_bal_flag' => true,
         ]);
 
