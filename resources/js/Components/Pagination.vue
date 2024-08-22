96382f98f8fcb9514788e5240d77162e297dbc44
tmwcl <tmwclaxton@gmail.com>
2024-08-22 11:06:17 +0100

    fix pagination

--- resources/js/Components/Pagination.vue
+++ resources/js/Components/Pagination.vue
@@ -1,22 +1,19 @@
 <template>
     <div>
-        <Component
-        :is="link.url ? 'Link' : 'span'"
-        v-for="link in links"
+        <a
         :href="link.url"
+        v-for="link in links"
         v-html="link.label"
         class="px-1"
-        :class="{'text-gray-500' : !link.url, 'font-bold text-white underline' : link.active}"
+        :class="{' ' : !link.url, 'font-bold underline' : link.active}"
         />
     </div>
 </template>
 
-<script>
-
-export default {
-    props:  {
-        links: Array
-    }
-}
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+    links: Array
+})
 
 </script>
