1ff438290034bbe13fd076f99130ac8ef9c3a63a
tmwcl <tmwclaxton@gmail.com>
2024-08-25 16:42:52 +0100

    fix bug

--- app/Http/Controllers/GraphController.php
+++ app/Http/Controllers/GraphController.php
@@ -10,20 +10,24 @@ class GraphController extends Controller
     public function index()
     {
         $dailyVolume = [];
+        $dailySatProfit = [];
         $offers = Offer::where('status', '14')->get();
         foreach ($offers as $offer) {
-            $dailyVolume[$offer->created_at->format('Y-m-d')][] = round($offer->amount);
+            $dailyVolume[$offer->created_at->format('Y-m-d')][] = round($offer->accepted_offer_amount);
+            $dailySatProfit[$offer->created_at->format('Y-m-d')][] = round($offer->accepted_offer_profit_sat);
         }
 
         // combine all the daily volumes
         foreach ($dailyVolume as $date => $volumes) {
             $dailyVolume[$date] = array_sum($volumes);
+            $dailySatProfit[$date] = array_sum($dailySatProfit[$date]);
         }
 
 
         return inertia('Graphs', [
             'dates' => array_keys($dailyVolume),
             'volumes' => array_values($dailyVolume),
+            'profits' => array_values($dailySatProfit),
         ]);
     }
 }
