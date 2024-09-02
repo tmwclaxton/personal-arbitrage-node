b8371a96c3b91e06d9f0044979637950a2cdc99c
tmwcl <tmwclaxton@gmail.com>
2024-09-02 20:59:45 +0100

    simple view changes

--- resources/js/Pages/Simple.vue
+++ resources/js/Pages/Simple.vue
@@ -43,33 +43,37 @@ const speed = ref('Normal');
         </div>
         <div class="border-b border-gray-200 mt-4 dark:border-gray-700"></div>
 
-        <div class="my-auto">
-            <!--input for type of offer (buy/sell)-->
-            <div class="flex justify-center  flex flex-row gap-x-2">
-                <p>{{ offerType ? 'Buy' : 'Sell' }}</p>
-                <ToggleButton v-model="offerType" :options="['Buy', 'Sell']"/>
-            </div>
+        <div class="my-auto flex flex-col gap-y-3">
+            <p class="text-2xl font-bold text-center mb-5 unde rline">Coming Soon...</p>
 
-            <!--input for amount of offer-->
-            <div class="flex justify-center mt-4">
-                <TextInput v-model="offerAmount" type="number" placeholder="No. of Satoshis"/>
-            </div>
+            <div class="opacity-50 pointer-events-none">
+                <!--input for type of offer (buy/sell)-->
+                <div class="flex justify-center  flex flex-row gap-x-2">
+                    <p>{{ offerType ? 'Buy' : 'Sell' }}</p>
+                    <ToggleButton v-model="offerType" :options="['Buy', 'Sell']"/>
+                </div>
 
-            <!--input for speed of offer-->
-            <div class="flex justify-center mt-4 mx-auto w-3620">
-                <!--['Slow', 'Normal', 'Fast', 'Extreme']" buttons?-->
-                <select v-model="speed"
-                        class="w-36 block mt-1 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm py-2 px-3 text-base focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
-                    <option value="Slow">Slow</option>
-                    <option value="Normal">Normal</option>
-                    <option value="Fast">Fast</option>
-                    <option value="Extreme">Extreme</option>
-                </select>
-            </div>
+                <!--input for amount of offer-->
+                <div class="flex justify-center mt-4">
+                    <TextInput v-model="offerAmount" type="number" placeholder="No. of Satoshis"/>
+                </div>
 
-            <!--start-->
-            <div class="flex justify-center mt-4">
-                <PrimaryButton @click="submit">Start</PrimaryButton>
+                <!--input for speed of offer-->
+                <div class="flex justify-center mt-4 mx-auto w-3620">
+                    <!--['Slow', 'Normal', 'Fast', 'Extreme']" buttons?-->
+                    <select v-model="speed"
+                            class="w-36 block mt-1 bg-white dark:bg-gray-800 border border-gray-300 dark:border-gray-700 rounded-md shadow-sm py-2 px-3 text-base focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                        <option value="Slow">Slow</option>
+                        <option value="Normal">Normal</option>
+                        <option value="Fast">Fast</option>
+                        <option value="Extreme">Extreme</option>
+                    </select>
+                </div>
+
+                <!--start-->
+                <div class="flex justify-center mt-4">
+                    <PrimaryButton @click="submit">Start</PrimaryButton>
+                </div>
             </div>
         </div>
 
