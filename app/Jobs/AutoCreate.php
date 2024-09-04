0ef9f4cb0f67dac77a62ffbc7aec35bc8acbe3c3
tmwcl <tmwclaxton@gmail.com>
2024-09-04 19:23:32 +0100

    ttl change so offers get removed when i go to sleep!

--- app/Jobs/AutoCreate.php
+++ app/Jobs/AutoCreate.php
@@ -46,6 +46,20 @@ class AutoCreate implements ShouldQueue
                 $count = Offer::where([['status', '<=', 3], ['posted_offer_template_id', $template->id]])->get()->count();
                 if ($template->quantity > $count) {
 
+                    // if $adminDashboard->scheduler is enabled, then we should adapt the ttl to when end time is if it goes over the end time
+                    if ($adminDashboard->scheduler) {
+                        // $adminDashboard->auto_accept_end_time ($end) is in 24 hour format like 21:00:00 so we need to convert it to a carbon date object of today
+
+                        $end = Carbon::parse($adminDashboard->auto_accept_end_time);
+                        $now = Carbon::now();
+                        $diff = $now->diffInSeconds($end);
+                        if ($diff < $template->ttl) {
+                            $template->ttl = $diff;
+                            // round to nearest hour in seconds with a minimum of 1 hour
+                            $template->ttl = max(3600, round($template->ttl / 3600) * 3600);
+                        }
+                    }
+
                     for ($i = 0; $i < $template->quantity - $count; $i++) {
                         $robosats = new \App\WorkerClasses\Robosats();
                         $response = $robosats->createSellOffer(
