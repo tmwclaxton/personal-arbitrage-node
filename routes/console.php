d5bbcdd719a7e977bfe1b6c734f002ee53bc8b0c
tmwcl <tmwclaxton@gmail.com>
2024-09-04 14:40:28 +0100

    change auto accept

--- routes/console.php
+++ routes/console.php
@@ -131,11 +131,6 @@ Schedule::command('auto:confirm-final')
     ->description('auto confirm final')
     ->everyMinute()->withoutOverlapping(1);
 
-// sitting offer detailer
-// Schedule::command('app:sitting-offer-detailer')
-//     ->description('sitting offer detailer')
-//     ->everyMinute()->withoutOverlapping(1);
-
 // ping umbrel check
 Schedule::command('app:umbrel-token-reset')
     ->description('reset umbrel token')
