6721478e040e84246bd7a05d8d5d5284c4e7e3ee
tmwcl <tmwclaxton@gmail.com>
2024-11-03 22:36:39 +0000

    profile settings

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -20,7 +20,7 @@ defineProps({
 
     <AuthenticatedLayout>
         <template #header>
-            <h2 class="font-semibold text-xl text-gray-800 leading-tight dark:text-gray-100">Profile</h2>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight dark:text-gray-100">Profile Settings</h2>
         </template>
 
         <div class="py-12">
