c1b840ff89b1176be204da967c4d53de5aed7f26
Joshua Young <joshuasy10mc@gmail.com>
2025-02-10 16:29:38 +0000

    add semicolon

--- app/RobosatsErrors.php
+++ app/RobosatsErrors.php
@@ -6,5 +6,5 @@ class RobosatsErrors
 {
 
     // !TODO: actually consult robosats api docs and get rest of the error messages or maybe some way to import them from their github repo
-    const COLLABORATIVELY_CANCELLED_ERROR = "This order has been cancelled collaborativelly"
+    const COLLABORATIVELY_CANCELLED_ERROR = "This order has been cancelled collaborativelly";
 }
